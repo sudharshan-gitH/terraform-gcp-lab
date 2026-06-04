3d5d4e80ba965bafd8f2be63b71e9059fd711053
Sudharshan <sudharshansunny64@gmail.com>
2026-06-04 17:13:57 +0530

    Configure GCS remote backend

--- provider.tf
+++ provider.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5"
 
+  backend "gcs" {
+    bucket = "tf-state-optical-carrier-498316-p8"
+    prefix = "lab"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
